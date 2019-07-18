a02908c8ca37f746a97227e1200a005f6224ee8c
funkynine <igorbevz11@icloud.com>
2019-07-19 00:00:19 +0200

    Fixed style

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -7,6 +7,7 @@ import { fromEvent } from 'rxjs';
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.sass']
 })
+
 export class PageComponent implements OnInit {
 
   images = ['../../assets/ione4.png',
@@ -28,13 +29,6 @@ export class PageComponent implements OnInit {
     this.subscription()
   }
 
-  // subscription() {
-  //   fromEvent(document, 'click')
-  //     .subscribe(() => {
-  //       this.checkStatus = !this.checkStatus;
-  //     });
-  // }
-
   subscription() {
     const halfHeight = (document.body.offsetHeight / 2) - (window.innerHeight * 1.5);
 
@@ -45,15 +39,5 @@ export class PageComponent implements OnInit {
         }
     });
   }
-
-  // private subscriber(): void {
-  //   const halfHeight = (this.document.body.offsetHeight / 2) - (window.innerHeight * 1.5);
-  //   this.scrollSubscription$ = fromEvent(window, 'scroll')
-  //     .subscribe(() => {
-  //       if (window.pageYOffset > halfHeight) {
-  //         this.createAccountModal();
-  //       }
-  //     });
-  // }
 
 }
