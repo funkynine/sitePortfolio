0e1a0137b418ca4ce3918da40e005741b496c33e
funkynine <igorbevz11@icloud.com>
2019-07-21 22:44:54 +0200

    work with project page

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -1,32 +1,37 @@
 export const projects = [
     {
         title: "Site portfolio",
-        image: "../../assets/ione1.png",
-        content: "1",
-        role: "Role: Front-end"
+        image: "../../assets/siteP.png",
+        content: "It's site for post my work",
+        role: "Role: Front-end",
+        link: "http://localhost:4200"
     },
     {
         title: "iOne",
         image: "../../assets/ione1.png",
         content: "I developed a landing page for our company ione.vn.ua",
-        role: "Role: Front-end"
+        role: "Role: Front-end",
+        link: "http://ione.vn.ua"
     },
     {
         title: "Mini store",
         image: "../../assets/miniShop1.png",
         content: "Example mini store on Angular.",
-        role: "Role: Front-end"
+        role: "Role: Front-end",
+        link: "https://github.com/funkynine/miniShop"
     },
     {
         title: "Wheater blog",
-        image: "../../assets/ione1.png",
+        image: "../../assets/weatherBlog.jpg",
         content: "I developed a landing page for our company.",
-        role: "Role: Front-end"
+        role: "Role: Full-stack",
+        link: "https://github.com/funkynine/CS50"
     },
     {
         title: "Layet page",
         image: "../../assets/proj2.png",
         content: "I developed a landing page for our company.",
-        role: "Role: Front-end"
+        role: "Role: Front-end",
+        link: "https://goo.gl/JqSJQs"
     },
 ]
