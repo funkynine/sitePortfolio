6dc518b966bac75dc51bcd1da558d17bb663dcf6
funkynine <igorbevz11@icloud.com>
2019-07-21 15:51:57 +0200

    Completed animation skillz on about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  checkStatus = false;
+  procent: Number;
+
+  
+
+
   constructor() { }
 
   ngOnInit() {
+    this.subscription()
   }
+
+  subscription() {
+    const halfHeight = (document.body.offsetHeight / 2) - (window.innerHeight * 1.5);
+
+    fromEvent(window, 'scroll')
+      .subscribe(() => {
+        if (window.pageYOffset > halfHeight) {
+          this.checkStatus = true;
+        }
+    });
+  }  
 
 }
